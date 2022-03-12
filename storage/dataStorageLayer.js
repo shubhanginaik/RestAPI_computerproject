98754da40475a24163aac5172b5eae5977648fa7
Shubhangi Naik <88744486+shubhanginaik@users.noreply.github.com>
2022-03-12 18:26:42 +0200

    Update dataStorageLayer.js

--- storage/dataStorageLayer.js
+++ storage/dataStorageLayer.js
@@ -57,7 +57,7 @@ module.exports = class Datastorage{
                 }
             } 
             catch(err){
-                reject(MESSAGES.PROGRAM_ERROR)
+                reject(MESSAGES.PROGRAM_ERROR())
             }
         })
     } // end of get
